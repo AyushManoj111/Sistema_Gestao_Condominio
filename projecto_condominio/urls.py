6e6abb3e3f9961d4aafe65cf9bb66b418a217375
Ayush Manoj <ayushmk69@gmail.com>
2025-11-23 11:17:08 +0200

    Docker commit 2

--- projecto_condominio/urls.py
+++ projecto_condominio/urls.py
@@ -3,10 +3,10 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
-from gerente_condominio.views import home
+from gerente_condominio.views import login_view
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('', login_view, name='login'),
     path('admin/', admin.site.urls),
     path('gerente/', include('gerente_condominio.urls')),
 ]
